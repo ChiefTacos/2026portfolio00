fef938d032d0ab06d00b0e27c883c4328134aaaf
Michael Murray <seipsirc@gmail.com>
2026-01-10 21:58:51 -0600

    overlay mobile sizing

--- src/components/Office.jsx
+++ src/components/Office.jsx
@@ -448,8 +448,8 @@ const ServiceWindowButton = ({ onClick, isClickable }) => {
                 // "
                 className={`overlay-window ${className} bg-white rounded-lg shadow-2xl border border-gray-700 
                 overflow-y-auto custom-scrollbar 
-  min-h-[110vh] max-h-[130vh] lg:min-h-[90vh] lg:max-h-[100vh]
-  w-[100vw] max-w-[1000px] md:w-[90vw] md:max-w-[1200px] lg:w-[70vw] lg:max-w-[1800px]`}
+  min-h-[150vh]  lg:min-h-[90vh] lg:max-h-[100vh]
+  w-[200vw] h-[200vw]  max-w-[1000px] md:max-w-[1200px] lg:w-[70vw] lg:max-w-[1800px]`}
 
                   onPointerDown={isVisible ? handleDragStart : undefined}
 
